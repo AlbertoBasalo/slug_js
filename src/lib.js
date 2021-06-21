b0c931f5eb392be1f781d04f06ee576713551bec
Alberto Basalo <albertobasalo@hotmail.com>
2021-06-21 16:49:54 +0200

    fix: eslint prettier config

--- src/lib.js
+++ src/lib.js
@@ -1,32 +1,36 @@
 export function getSlug(source, lenght = 100, timestamp, datestamp) {
-  const sourceString = String(source);
-  const noSpaces = replaceMarks(sourceString);
-  const noCurrencies = replaceCurrencies(noSpaces);
-  const truncated = truncate(noCurrencies, lenght);
-  let signed = truncated;
+  let cleanSource = String(source)
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .join("-")
+    .split("’")
+    .join("")
+    .split("’")
+    .join("")
+    .split("?")
+    .join("")
+    .split(",")
+    .join("")
+    .split(":")
+    .join("")
+    .split(".")
+    .join("")
+    .split("#")
+    .join("")
+    .split("+")
+    .join("")
+    .replace("$", "dollar")
+    .replace("€", "euros");
+
+  if (cleanSource.length > lenght) {
+    cleanSource = cleanSource.slice(0, lenght);
+  }
   if (timestamp) {
-    signed += "_" + Date.now()
+    cleanSource += "_" + Date.now();
   }
   if (datestamp) {
-    signed += "_" + new Date().toISOString().slice(0,10)
+    cleanSource += "_" + new Date().toISOString().slice(0, 10);
   }
-  const result = signed.toLowerCase();
-  return result;
-}
-
-function replaceMarks(source) {
-  const result = source.split(' ').join('-').split('’').join('').split('’').join('').split('?').join('').split(',').join('').split(':').join('').split('.').join('').split('#').join('').split('+').join('');
-  return result;
-}
-
-function replaceCurrencies(source) {
-  const  result = source.replace('$','dollar').replace('€','euros');
-  return result;
-}
-
-function truncate(source, lenght = 60) {
-  if (source.length <= lenght) {
-    return source
-  }
-  return source.slice(0, lenght) + '...'
+  return cleanSource;
 }
--- src/main.js
+++ src/main.js
@@ -10,17 +10,28 @@ console.log(getSlug("What is a slug and how to optimize it?"));
 
 console.log(getSlug("The COVID-19, coronavirus desease, is being defeated"));
 
-console.log(getSlug("Design Patterns: Top 5 Techniques for Implementing Fault Tolerance in Distributed Systems"));
+console.log(
+  getSlug(
+    "Design Patterns: Top 5 Techniques for Implementing Fault Tolerance in Distributed Systems"
+  )
+);
 
+console.log(
+  getSlug(
+    "Running our test suite with Mocha took 12+ minutes. In CI with our beefy build machines we’re now able to run the entire Jest suite in 4 minutes 30 seconds"
+  )
+);
 
-console.log(getSlug('Running our test suite with Mocha took 12+ minutes. In CI with our beefy build machines we’re now able to run the entire Jest suite in 4 minutes 30 seconds'))
+console.log(
+  getSlug(
+    "When should I migrate my Frontend to an architecture based on microfrontends?",
+    undefined,
+    true
+  )
+);
 
-console.log(getSlug("When should I migrate my Frontend to an architecture based on microfrontends?", undefined, true));
-
-console.log(getSlug("Solve Module Import Aliasing for Webpack, Jest, and VSCode", undefined, false, true));
-
-
+console.log(
+  getSlug("Solve Module Import Aliasing for Webpack, Jest, and VSCode", undefined, false, true)
+);
 
 console.log(getSlug("What Is A Smart Contract And How Does It Work ?", 30, true));
-
-
