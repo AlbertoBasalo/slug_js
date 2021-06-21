ee9c9022b2553a85a06ae1382fcf0f893cfe3eb7
Alberto Basalo <albertobasalo@hotmail.com>
2021-06-21 14:32:49 +0200

    raw implementation

--- src/lib.js
+++ src/lib.js
@@ -1,3 +1,32 @@
-export function getGreetings(name) {
-  return `Hello ${name}, welcome to the Javascript world!`;
+export function getSlug(source, lenght = 100, timestamp, datestamp) {
+  const sourceString = String(source);
+  const noSpaces = replaceMarks(sourceString);
+  const noCurrencies = replaceCurrencies(noSpaces);
+  const truncated = truncate(noCurrencies, lenght);
+  let signed = truncated;
+  if (timestamp) {
+    signed += "_" + Date.now()
+  }
+  if (datestamp) {
+    signed += "_" + new Date().toISOString().slice(0,10)
+  }
+  const result = signed.toLowerCase();
+  return result;
+}
+
+function replaceMarks(source) {
+  const result = source.split(' ').join('-').split('’').join('').split('’').join('').split('?').join('').split(',').join('').split(':').join('').split('.').join('').split('#').join('').split('+').join('');
+  return result;
+}
+
+function replaceCurrencies(source) {
+  const  result = source.replace('$','dollar').replace('€','euros');
+  return result;
+}
+
+function truncate(source, lenght = 60) {
+  if (source.length <= lenght) {
+    return source
+  }
+  return source.slice(0, lenght) + '...'
 }
